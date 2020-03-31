cae35ad39ca163383080a44fea6c60468689e1e2
aashra <aashrarayamajhi@gmail.com>
2020-03-31 10:36:53 -0500

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-<<<<<<< HEAD
+
         System.out.println("hello world !from github.com  ");
         System.out.println(("Aashra kc "));
-=======
-        System.out.println("hello world ! from github.com..");
-        System.out.print("Sandesh Lamsal");
+
 	
->>>>>>> 3ef8de3e4dd3c79b8821021c9f582b118f1b4a31
+
     }
 }
